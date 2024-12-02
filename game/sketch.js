0d0702fc142324cef9ab984e649ffb2ff84910b1
Danielitosk <d.santiago07@hotmail.com>
2024-12-02 14:26:51 -0600

    borderlines
    established the borders where the player can not go.

---
+++ game/sketch.js
@@ -0,0 +1,78 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let x = 0;
+let y = 0;
+let playerX = 0;
+let playerY = 0;
+let hero;
+let player;
+
+function preload() {
+  forest = loadImage('assets/background.webp');
+  player = loadImage('assets/maincharacter.jpg');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  hero = new Character();
+}
+
+
+function draw() {
+  background(220);
+  imageMode(CENTER);
+  image(forest, windowWidth / 2, windowHeight / 2, windowWidth + 1, windowHeight);
+  hero.display();
+  hero.move();
+}
+
+class Character {
+  constructor(x, y, move) {
+    this.x = width / 2;
+    this.y = height / 2;
+
+  }
+
+  display() {
+    rectMode(CENTER);
+    rect(this.x, this.y + 45, 30, 60);
+    ellipse(this.x, this.y, 45);
+  }
+
+  move() {  // player movement and check for borders in the canvas 
+
+    if (keyIsPressed) {
+      if (keyCode === 68) {  // move to the right
+        this.x += 6;
+        if (this.x >= windowWidth - 20) {     // check right border
+          this.x = windowWidth - 20;
+        }
+      }
+      if (keyCode === 65) {  // move to the left
+        this.x -= 6;
+        if (this.x <= 20) {     // check left border
+          this.x = 20;      
+        }
+      }
+      if (keyCode === 83) {  // move down
+        this.y += 6;
+        if (this.y >= windowHeight - 40) {   // check lower border
+          this.y = windowHeight-40;
+        }
+      }
+      if (keyCode === 87) {  //move up
+        this.y -= 6;
+        if (this.y <=  20) {    // check upper border
+          this.y = 20;
+        }
+      }
+    }
+  }
+}
+
+// this.x>20
